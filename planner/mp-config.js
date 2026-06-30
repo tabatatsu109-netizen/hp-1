a8578f0512f4373cbe1081b80d362f4f6b520d74
tabatatsu109-netizen <tabatatsu109@gmail.com>
2026-06-30 14:14:16 +0900

    Add Match Planner to planner/ subfolder

---
+++ planner/mp-config.js
@@ -0,0 +1,9 @@
+// ★ Match Planner 設定ファイル ★
+// firebaseSecret は各端末の「設定」画面から入力してください（このファイルには書かない）
+
+const MP_CONFIG = {
+  clubName:       'GRANDE FC',
+  clubId:         'grande-fc',
+  firebaseUrl:    'https://hp-1-d7bce-default-rtdb.asia-southeast1.firebasedatabase.app',
+  firebaseSecret: '',  // 設定画面から入力 → LocalStorage に保存される
+};
